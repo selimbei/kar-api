89a016d209d1205dcef04602e5ddf17e911ca759
selimbei <tohir.wahyudi@gmail.com>
2024-05-08 14:51:54 +0700

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+
+const app: Application = express()
+const port: Number = 4000
+
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(400).send({ status: 400, message: "i'am sick" })
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
