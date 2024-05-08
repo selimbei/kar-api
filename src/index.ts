37b28e6270f8272d0cbfae10f4fd873e87313aae
selimbei <tohir.wahyudi@gmail.com>
2024-05-08 15:36:07 +0700

    Setup prettier, eslint, husky

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 const port: Number = 4000
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-  res.status(400).send({ status: 400, message: "i'am sick" })
+  res.status(400).send({ status: 400, data: "i'am sick" })
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
