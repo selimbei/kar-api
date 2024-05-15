f8503333931fe2563611b42529e6c739bc55ec41
selimbei <tohir.wahyudi@gmail.com>
2024-05-15 22:42:57 +0700

    add: requireUser for get worker by name

--- src/routes/worker.route.ts
+++ src/routes/worker.route.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { createWorker, getWorker } from '../controller/worker.controller'
-import { requireAdmin } from '../middleware/auth'
+import { requireAdmin, requireUser } from '../middleware/auth'
 
 export const WorkerRouter: Router = Router()
 
 WorkerRouter.get('/', getWorker)
-WorkerRouter.get('/:name', getWorker)
+WorkerRouter.get('/:name', requireUser, getWorker)
 WorkerRouter.post('/', requireAdmin, createWorker)
 WorkerRouter.patch('/:name', createWorker)
